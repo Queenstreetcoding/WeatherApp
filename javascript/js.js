3e071610ac850bbfc8cda555c44af2a27c3288e0
Cindy <cindy_verboog@hotmail.com>
2022-12-27 17:23:58 +0100

    Added part of search engine

--- javascript/js.js
+++ javascript/js.js
@@ -1,33 +1,32 @@
-let apiKey = "d491caa2745b3f084379b6tba1a6oba9";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Delft&key=d491caa2745b3f084379b6tba1a6oba9&units=metric";
 
+function showTemperature (response){
+    let dateElement = document.querySelector ("#date");
+    let cityElement = document.querySelector ("#city");
+    let condition = document.querySelector ("#condition");
+    let windElement = document.querySelector ("#wind");
+    let humidityElement = document.querySelector ("#humidity");
+    let topTemperatureElement = document.querySelector ("#degreesTop");
 
-let now = new Date ();
-let currentDay = now.getDay ();
-if (currentDay < 10){
-    currentDay = `0${currentDay}`;
+    cityElement.innerHTML = response.data.city;
+    condition.innerHTML = response.data.condition.description;
+    windElement.innerHTML = Math.round (response.data.wind.speed);
+    humidityElement.innerHTML = Math.round (response.data.temperature.humidity);
+    topTemperatureElement.innerHTML = Math.round (response.data.temperature);
 }
-let currentMonth = now.getMonth ();
-if (currentMonth < 10){
-    currentMonth = `0${currentDay}`;
+
+function search (city){
+    let apiKey = "d491caa2745b3f084379b6tba1a6oba9";
+    let apiUrl = `https//api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);    
 }
-let currentYear = now.getFullYear();
 
-dateElement = document.querySelector ("#showDate");
-dateElement.innerHTML = `${currentDay}-${currentMonth}-${currentYear}`;
+function handleSubmit (event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector ("#city-input");
+    search (cityInputElement.value);
+}
 
+let form = document.querySelector ("#search-form");
+form.addEventListener ("submit", handleSubmit);
 
-//let day = days[now.getDay()];
-//let days = [
-  //  "Sunday",
-  //  "Monday",
-  //  "Tuesday",
-  //  "Wednesday",
-  //  "Thursday",
-  //  "Friday",
-  //  "Satureday"
- // ];
-  
- // daysElement = document.querySelector (".day-bottom");
- // daysElement.innerHTML = `${day}`;\
-
+search = ("New York");

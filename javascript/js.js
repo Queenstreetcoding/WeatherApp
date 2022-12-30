1a43ae3c25812fb93ba04a645f7d3ee7160085be
Verboog <c.verboog@fugro.com>
2022-12-30 16:10:41 +0100

    Downloaded icons and implemented icons in code

--- javascript/js.js
+++ javascript/js.js
@@ -30,7 +30,6 @@ function showDay (timestamp) {
 
 function showForecast (response){
   let forecast = response.data.daily;
-  console.log (response.data);
   let forecastElement = document.querySelector ("#forecast");
   
   let forecastHTML = `<div class="row">`;
@@ -41,7 +40,7 @@ function showForecast (response){
           <div class="day-bottom">
               ${showDay (forecastDay.time)}
           </div>
-          <img src="https://img.icons8.com/stickers/200/null/sun.png" alt="Sunny" class ="imgsmall-bottom" width="80"/>
+          <img src="img/${forecastDay.condition.icon}.png" alt="Sunny" class ="imgsmall-bottom" width="80"/>
           <div class="degrees-bottom">
           <span class="degrees-bottom-max">${Math.round(forecastDay.temperature.maximum)}°</span>
           <span class="degrees-bottom-min">${Math.round(forecastDay.temperature.minimum)}°</span>  
@@ -58,12 +57,14 @@ function getCoordinates (coordinates) {
 }
 
 function showTemperature (response){
+  console.log (response);
     let cityElement = document.querySelector ("#city");
     let countryElement = document.querySelector ("#country");
     let condition = document.querySelector ("#condition");
     let windElement = document.querySelector ("#wind");
     let humidityElement = document.querySelector ("#humidity");
     let topTemperatureElement = document.querySelector ("#degreesTop");
+    let iconElement = document.querySelector ("#main-icon");
 
     celsiusTemperature = response.data.temperature.current;
 
@@ -73,6 +74,7 @@ function showTemperature (response){
     windElement.innerHTML = Math.round (response.data.wind.speed);
     humidityElement.innerHTML = Math.round (response.data.temperature.humidity);
     topTemperatureElement.innerHTML = Math.round (celsiusTemperature);
+    iconElement.setAttribute ("src", `img/${response.data.condition.icon}.png`);
 
     getCoordinates(response.data.coordinates);
 }
@@ -89,28 +91,7 @@ function handleSubmit (event){
     search (cityInputElement.value);
 }
 
-function fahrenheitConversion (event){
-    event.preventDefault ();
-    let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
-    let temperatureElement = document.querySelector ("#degreesTop");
-    temperatureElement.innerHTML = Math.round (fahrenheitTemperature);
-}
-
-function celsiusConversion (event){
-    event.preventDefault ();
-    let temperatureElement = document.querySelector ("#degreesTop");
-    temperatureElement.innerHTML = Math.round (celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector ("#search-form");
 form.addEventListener ("submit", handleSubmit);
 
 search ("Delft");
-
-let fahrenheitLink = document.querySelector ("#fahrenheit-link");
-fahrenheitLink.addEventListener ("click", fahrenheitConversion);
-
-let celsiusLink = document.querySelector ("#celsius-link");
-celsiusLink.addEventListener ("click", celsiusConversion);

c49537623555a5d6df978e35cd15180d2dfd57ec
Verboog <c.verboog@fugro.com>
2023-02-02 16:51:30 +0100

    Adjusted typo

--- javascript/js.js
+++ javascript/js.js
@@ -23,7 +23,7 @@ time.innerHTML = `${hours}:${minutes}`;
 function showDay (timestamp) {
  let date = new Date (timestamp * 1000);
  let day = date.getDay ();
- let days = ["Sunday", "Monday", "Tueday", "Wedday", "Thuday", "Friday", "Saturday"];
+ let days = ["Sunday", "Monday", "Tueday", "Wedday", "Thursday", "Friday", "Saturday"];
  
  return days[day];
 }
